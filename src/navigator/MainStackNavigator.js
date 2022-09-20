e31adbc68999cd0612a2423b43e4e71a602a1344
Ibrahim Sluma <mrlibya003@gmail.com>
2022-09-20 18:37:05 +0200

    + init navigators

---
+++ src/navigator/MainStackNavigator.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { HomeScreen } from '../screens';
+
+const Stack = createNativeStackNavigator();
+
+function MainStackNavigator() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} options={{headerShown:false}}/>
+        </Stack.Navigator>
+    );
+}
+
+export default MainStackNavigator;
